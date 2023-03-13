c03310ff339896cad35c98d06dc2e668e88a669a
IgoorToorres <115846114+IgoorToorres@users.noreply.github.com>
2023-03-13 10:52:51 -0300

    alguns exercicios

---
+++ curso_udemy/aula10/teste.js
@@ -0,0 +1,5 @@
+const n1 =5
+
+const resultado = n1 >= 5 ? 'numero maior que 5' : 'numero menor que 5'
+
+console.log(resultado)
--- curso_udemy/aula9/assets/js/main.js
+++ curso_udemy/aula9/assets/js/main.js
@@ -1,37 +1,60 @@
 const formulario = document.querySelector('#formulario')
 
-formulario.addEventListener('submit', function(evento){
-    evento.preventDefault()
-    const form_peso = evento.target.querySelector('#peso')
-    const form_altura = evento.target.querySelector('#altura')
 
-    const peso = Number(form_peso.value)
-    const altura = Number(form_altura.value)
+formulario.addEventListener('submit', function(event){
+    event.preventDefault()
+    const inputPeso = event.target.querySelector('#peso')
+    const inputAltura = event.target.querySelector('#altura')
 
-    if (!peso){
-        setResultado('peso invalido!', false )
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
+
+    if (!peso) {
+        setResultado('peso invalido!', false)
         return
     }
 
-    if(!altura){
-        setResultado('altura invalida!', false)
+    if (!altura) {
+        setResultado('altura invalida', false)
         return
     }
+    
+    const imc = getImc(peso, altura)
+    const nivelImc = getNivelImc(imc)
 
+    const msg = `Seu imc é ${imc} (${nivelImc})`
 
-    console.log('cheguei aqui!')
+    setResultado(msg, true)
 })
 
-function ciraP(){
-    const p = document.createElement('p')
-    return p
+function getNivelImc(imc){
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+
+    if (imc >= 39.9) return nivel[5]
+    if (imc >= 34.9) return nivel[4]
+    if (imc >= 29.9) return nivel[3]
+    if (imc >= 24.9) return nivel[2]
+    if (imc >= 18.75) return nivel[1]
+    if (imc < 18.75) return nivel[0]
 }
+
+
+function getImc(peso, altura){
+    const imc = peso / altura ** 2 
+    return imc.toFixed(2)
+}
+
 
 function setResultado(msg, isValid){
     const resultado = document.querySelector('.resultado')
     resultado.innerHTML = ''
-    const p = ciraP()
+    const p = document.createElement('p')
+
+    if (isValid){
+        p.classList.add('paragrafo_resultado')
+    }else{
+        p.classList.add('bad')
+    }
     p.innerHTML = msg
     resultado.appendChild(p)
 }
-
